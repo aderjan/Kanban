ad1b70cfa47b06c61ab67e57fccfd8707cb7b3e4
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-03-08 23:05:05 +0100

    First Commit

---
+++ Board.js
@@ -0,0 +1,37 @@
+var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+var myHeaders = {
+  'X-Client-Id': "1552",
+  'X-Auth-Token': "648c7ba14c85004681790d111096b32f"
+};
+
+var board = {
+	name: 'Tablica Kanban',
+	createColumn: function(column) {
+	  this.element.append(column.element);
+	  initSortable();
+	},
+	element: $('#board .column-container')
+};
+
+$('.create-column')
+    .click(function() {
+        var columnName = prompt('Wpisz nazwę kolumny');
+        $.ajax({
+    		url: baseUrl + '/column',
+    		method: 'POST',
+    		data: {
+            	name: columnName
+    		},
+    		success: function(response){
+    			var column = new Column(response.id, columnName);
+    			board.createColumn(column);
+          	}
+        });
+});
+	
+function initSortable() {
+    $('.card-list').sortable({
+      connectWith: '.card-list',
+      placeholder: 'card-placeholder'
+    }).disableSelection();
+  }
